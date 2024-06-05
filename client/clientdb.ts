11ef430b2885d342d7c1a0a5cd71b8cb219e43e5
andycontreras <andresconvar@gmail.com>
2024-06-04 23:01:31 -0300

    Updated pages

--- client/clientdb.ts
+++ client/clientdb.ts
@@ -3,7 +3,14 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = JSON.parse(process.env.FIREBASE_CREDENTIAL as any);
-
 const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
-export const _API = process.env.PORT || 'http://localhost:3005';
+const db = getDatabase(app);
+
+let _API: string;
+if (process.env.NODE_ENV == 'production') {
+	_API = '';
+} else {
+	_API = 'http://localhost:3005';
+}
+
+export { _API, db };
